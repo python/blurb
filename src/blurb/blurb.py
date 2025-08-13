e127462043210571512f1ee72ed741c6c34fde18
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-08-13 02:01:15 +0100

    Move 'blurb populate' to ``blurb._populate``

--- src/blurb/blurb.py
+++ src/blurb/blurb.py
@@ -475,26 +475,6 @@ def error(*a):
 
 
 @subcommand
-def populate():
-    """
-Creates and populates the Misc/NEWS.d directory tree.
-    """
-    os.chdir("Misc")
-    os.makedirs("NEWS.d/next", exist_ok=True)
-
-    for section in sections:
-        dir_name = sanitize_section(section)
-        dir_path = f"NEWS.d/next/{dir_name}"
-        os.makedirs(dir_path, exist_ok=True)
-        readme_path = f"NEWS.d/next/{dir_name}/README.rst"
-        with open(readme_path, "wt", encoding="utf-8") as readme:
-            readme.write(f"Put news entry ``blurb`` files for the *{section}* section in this directory.\n")
-        git_add_files.append(dir_path)
-        git_add_files.append(readme_path)
-    flush_git_add_files()
-
-
-@subcommand
 def export():
     """
 Removes blurb data files, for building release tarballs/installers.
